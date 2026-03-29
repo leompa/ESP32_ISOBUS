440e8609bd43a227aa1697a3f6b9e34c75428517
Leandro Abel Marchiori <leompa2016@gmail.com>
2026-03-29 10:22:15 -0300

    pos neged

--- src/rpm_counter.cpp
+++ src/rpm_counter.cpp
@@ -20,6 +20,12 @@ volatile uint64_t prev_pulse_time[NUM_SENSORS] = {0};
 static void IRAM_ATTR isr0(void* arg)
 {
     uint64_t now = esp_timer_get_time();
+    
+    // Anti-ruido: ignorar pulsos más rápidos que ~6000-8000 RPM
+    if (now - last_pulse_time[0] < 4000ULL) {  
+        return;
+    }
+    
     prev_pulse_time[0] = last_pulse_time[0];
     last_pulse_time[0] = now;
 }
@@ -27,6 +33,12 @@ static void IRAM_ATTR isr0(void* arg)
 static void IRAM_ATTR isr1(void* arg)
 {
     uint64_t now = esp_timer_get_time();
+    
+     // Anti-ruido: ignorar pulsos más rápidos que ~6000-8000 RPM
+    if (now - last_pulse_time[1] < 4000ULL) {  
+        return;
+    }
+
     prev_pulse_time[1] = last_pulse_time[1];
     last_pulse_time[1] = now;
 }
@@ -34,6 +46,10 @@ static void IRAM_ATTR isr1(void* arg)
 static void IRAM_ATTR isr2(void* arg)
 {
     uint64_t now = esp_timer_get_time();
+     // Anti-ruido: ignorar pulsos más rápidos que ~6000-8000 RPM
+    if (now - last_pulse_time[2] < 4000ULL) {  
+        return;
+    }
     prev_pulse_time[2] = last_pulse_time[2];
     last_pulse_time[2] = now;
 }
@@ -56,8 +72,7 @@ static void IRAM_ATTR isr4(void* arg)
 void rpm_init()
 {
     gpio_config_t io_conf;
-
-    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
@@ -88,7 +103,7 @@ int rpm_get(int index)
     uint64_t t2 = last_pulse_time[index];
 
     // si hace más de 1 segundo que no hay pulsos
-    if(now - t2 > 1000000)
+    if(now - t2 > 2000000)
         return 0;
 
     if(t1 > 0 && t2 > t1)
